6335b9bb752ebf37eddf88a8bda24f7b09d7f281
madiyarmoldakhmet <madiyarmoldakhmet@gmail.com>
2026-08-22 10:30:02 +0500

    feat: atomic dev update 10:30:02

---
+++ test/call_service_test.dart
@@ -0,0 +1,34 @@
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:svinobook/services/call_service.dart';
+
+void main() {
+  test('rejectCall marks an incoming call as rejected', () async {
+    final firestore = FakeFirebaseFirestore();
+    await firestore.collection('calls').doc('call-1').set({'status': 'calling'});
+    final service = CallService(firestore: firestore);
+
+    await service.rejectCall('call-1');
+
+    expect((await firestore.collection('calls').doc('call-1').get()).data()?['status'], 'rejected');
+  });
+
+  test('endCall marks an active call as ended', () async {
+    final firestore = FakeFirebaseFirestore();
+    await firestore.collection('calls').doc('call-2').set({'status': 'connected'});
+    final service = CallService(firestore: firestore);
+
+    await service.endCall('call-2');
+
+    expect((await firestore.collection('calls').doc('call-2').get()).data()?['status'], 'ended');
+  });
+
+  test('unauthenticated users cannot start a call', () async {
+    final service = CallService(firestore: FakeFirebaseFirestore());
+
+    expect(
+      () => service.makeCall('callee-1'),
+      throwsA(isA<StateError>()),
+    );
+  });
+}
